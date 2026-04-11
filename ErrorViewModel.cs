da756614b579df9737e7e6a019eb339fe12ae634
Vitoriadev83 <vitoriacdossantos83@gmail.com>
2026-04-11 17:27:46 -0300

    Primeiro programa em Python

---
+++ ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MeninaMimada.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ hello_world.py
@@ -0,0 +1,3 @@
+print ("Hello world!")
+git config --global user.name "Vitoriadev83"
+git config --global user.email "vitoriacdossantos73@gmail.com"
