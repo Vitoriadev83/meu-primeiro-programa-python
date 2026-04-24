bf87070e89992defd5a1660632f5035114213b0a
Vitoriadev83 <vitoriacdossantos73@gmail.com>
2026-04-24 13:07:38 -0300

    Delete ErrorViewModel.cs

--- ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MeninaMimada.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
